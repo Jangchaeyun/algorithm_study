2b94779aaec06bd0baeeaa7491eea111d5306e56
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2023-02-04 14:30:54 +0900

    Java 4일차

---
+++ java/4일차/1546번/src/Main2.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main2 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int max = -1;
+		double sum = 0.0;
+		
+		for (int i = 0; i < N; i++) {
+			int value = Integer.parseInt(st.nextToken());
+			
+			if (value > max) {
+				max = value;
+			}
+			
+			sum += value;
+		}
+		System.out.println(((sum / max) * 100.0) / N);
+	}
+}
