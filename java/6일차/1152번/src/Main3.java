d55b1779bcac2380a6d4b2762ca81e0c1b6d7289
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2023-02-06 13:05:10 +0900

    Java 6일차

---
+++ java/6일차/1152번/src/Main3.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+
+public class Main3 {
+
+	public static void main(String[] args) throws IOException {
+		
+		int count = 0;
+		int pre_str = 32;
+		int str;
+		
+		while(true) {
+			str = System.in.read();
+			
+			if (str == 32) {
+				if (pre_str != 32) count++;
+			}
+			
+			else if (str == 10) {
+				if (pre_str != 32) count++;
+				break;
+			}
+			pre_str = str;
+		}
+		
+		System.out.println(count);
+	}
+}
