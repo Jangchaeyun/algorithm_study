ea2074ed1394732c1c4572bedc591c81fbae2835
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2023-02-16 18:33:29 +0900

    Java 16일차

---
+++ java/16일차/1541번/src/Main2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+ 
+public class Main2 {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		int sum = Integer.MAX_VALUE;
+		StringTokenizer subtraction = new StringTokenizer(in.nextLine(), "-");
+ 
+		while (subtraction.hasMoreTokens()) {
+			int temp = 0;
+			StringTokenizer addition = new StringTokenizer(subtraction.nextToken(), "+");
+			while (addition.hasMoreTokens()) {
+				temp += Integer.parseInt(addition.nextToken());
+			}
+			if (sum == Integer.MAX_VALUE) {
+				sum = temp;
+			} else {
+				sum -= temp;
+			}
+		}
+		System.out.println(sum);
+	}
+}
