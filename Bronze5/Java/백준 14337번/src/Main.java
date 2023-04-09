d208e200483bb0b8a8ab82b4d4cf64e467290d19
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2023-04-09 11:28:22 +0900

    Bronze5 Java 백준 4일차

---
+++ Bronze5/Java/백준 14337번/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("===================\n"
+				+ "          ____||___\n"
+				+ "\\ /      /       []\\\n"
+				+ " X=======           \\__\n"
+				+ "/ \\      \\____________|\n"
+				+ "            ||  ||\n"
+				+ "         \\-----------/");
+	}
+
+}
