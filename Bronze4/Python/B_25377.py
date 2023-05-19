b6212da435d35e567bb6bbca9909e5e22a601ddf
zzangsally <zzangsally@naver.com>
2023-05-19 10:00:34 +0900

    Bronze4 Py 0519

---
+++ Bronze4/Python/B_25377.py
@@ -0,0 +1,14 @@
+inf = 100000000
+ans = inf
+
+for i in range(int(input())):
+    x, y = map(int, input().split())
+
+    if x <= y:
+        ans = min(ans, y)
+
+if ans == inf:
+    print(-1)
+
+else:
+    print(ans)
